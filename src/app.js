ea64712fc855563cadb52faef4b29b5ab0827ed6
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-05-25 14:25:27 -0300

    fix: Refatoração do código para o modelo de arquitetura MVC

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import livros from "./models/Livro.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => {
@@ -11,28 +12,11 @@ const app = express();
 
 app.use(express.json());
 
-// const livros = [
-//     {id: 1, "titulo": "Senhor dos aneis"}, {id: 2, "titulo": "O Hobbit"}
-// ]
-
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node');
-})
-
-app.get('/livros', (req, res) => {
-    livros.find((err, livros) => {
-        res.status(200).json(livros);
-    });
-})
+routes(app);
 
 app.get('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id);
     res.json(livros[index]);
-})
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro foi cadastrado com sucesso.');
 })
 
 app.put('/livros/:id', (req, res) => {
