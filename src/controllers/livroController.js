a0b62bee4c3411a5bdd9312f4697624fc2ec0fd7
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-05-25 14:27:02 -0300

    feat: Criação da classe que será o controlador das requisições, foi criado os metódos que serão utilizados nas requisições get e post da nossa aplicação de teste

---
+++ src/controllers/livroController.js
@@ -0,0 +1,22 @@
+import livros from "../models/Livro.js";
+
+class LivroController {
+    static listBooks = (req, res) => {
+        livros.find((err, livros) => {
+            res.status(200).json(livros);
+        });
+    }
+
+    static postBook = (req, res) => {
+        let livro = new livros(req.body);
+        livro.save((err) => {
+            if (err) {
+                res.status(500).send({message: `${err.message} - falha ao cadastrar o livro.`})
+            } else {
+                res.status(201).send(livro.toJSON())
+            }
+        })
+    }
+}
+
+export default LivroController
